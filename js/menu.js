7a6bb967469ad92a1738265ccd3891c5a7183da6
Justin Duong <jduong@umich.edu>
2022-05-02 14:19:21 -0400

    Worked on design for product-modal.

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Header navigation
   const menuHamburger = document.getElementById('menu-hamburger');
   const menuClose = document.getElementById('menu-close');
   const menuList = document.querySelector('.menu-list');
+  // Backdrop
   const backdrop = document.querySelector('.backdrop');
+  // Main
+  const ctaBtn = document.getElementById('btn-cta');
+  
 
   menuHamburger.addEventListener('click', () => {
     menuHamburger.classList.add('hide');
@@ -16,5 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
     menuClose.classList.add('hide');
     menuList.classList.add('hide');
     backdrop.classList.add('hide');
+  });
+
+  ctaBtn.addEventListener('click', () => {
+    const projectModal = document.querySelector('.project-modal');
+    projectModal.classList.remove('hide');
+    backdrop.classList.remove('hide');
   });
 });
